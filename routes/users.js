ddf17046a7e3d0c3b2131b2994266f56834a82bf
huangmengyao <2594838054@qq.com>
2024-02-06 20:31:57 +0800

    feat: 升级小任务

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,24 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send(
-    [1,2,3,4,5]
-  );
+  res.send([
+    {
+      "name": "hmy",
+      "age": 18,
+      "hobby": [
+        { "label": "game", "time": "2023-1-1" },
+        { "label": "study", "time": "2022-10-1" }
+      ]
+    },
+    {
+      "name": "ckj",
+      "age": 21,
+      "hobby": [
+        { "label": "coding", "time": "2022-8-1" },
+        { "label": "love", "time": "2022-2-1" }
+      ]
+    }
+  ]);
 });
 
 module.exports = router;
